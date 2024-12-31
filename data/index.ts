e5cc942901411c9da9f1a30b0aac1a033626f70e
Didul Adeesha <117925590+didul123@users.noreply.github.com>
2024-12-31 12:23:23 +0530

    didul-portfolio

---
+++ data/index.ts
@@ -0,0 +1,147 @@
+// SEO Optimized Navigation Items
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+// Grid Items with Optimized Titles
+export const gridItems = [
+  {
+    id: 1,
+    title: "Collaborative UI/UX design approach for seamless communication",
+    description: "", // Consider adding a meta description for SEO
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Adaptable to various time zones ",
+    description: "", // Add a concise meta description here
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast passionate about UI/UX design and development",
+    description: "", // SEO meta description could enhance engagement
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 6,
+    title: "Ready to start your next UI/UX project? Let’s collaborate!",
+    description: "", // Consider adding a strong call-to-action description
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+// Projects with Improved Descriptions and Keywords
+export const projects = [
+  {
+    id: 1,
+    title: "Digital marketing agency",
+
+    img: "/p1.png",
+    iconLists: ["/figma.png", "/framer2.png", "/ps.png", "/wf.png"],
+  },
+  {
+    id: 2,
+    title: "Merch design company",
+
+    img: "/p2.png",
+    iconLists: ["/figma.png", "/framer2.png", "/ps.png", "/wf.png"],
+  },
+  {
+    id: 3,
+    title: "Merch Selling App",
+
+    img: "/p3.png",
+    iconLists: ["/figma.png", "/framer2.png", "/ps.png", "/wf.png"],
+  },
+  {
+    id: 4,
+    title: "Pet Care App",
+
+    img: "/p4.png",
+    iconLists: ["/figma.png", "/framer2.png", "/ps.png", "/wf.png"],
+  },
+];
+
+// Testimonials with Enhanced Variations for Authenticity
+export const testimonials = [
+  {
+    quote:
+      "It was a real delight to work with Didul Adeesha. Attention to detail and bringing our vision into reality has been beyond what we had imagined. Our website has gained so much more user experience, and they just absolutely love it!",
+    name: "Kalana Didulanga",
+    title: "Full Stack Developer",
+  },
+  {
+    quote:
+      "The dedication Adrian brings to every project is inspiring. His expertise in UI/UX design elevated our digital platform beyond expectations.",
+    name: "Shehan hansaka",
+    title: "Back End Developer",
+  },
+];
+
+// Companies with Improved Readability
+export const companies = [
+  { id: 1, name: "Figma", img: "/cloud.svg", nameImg: "/cloudName.svg" },
+  { id: 2, name: "Appwrite", img: "/app.svg", nameImg: "/appName.svg" },
+  { id: 3, name: "Hostinger", img: "/host.svg", nameImg: "/hostName.svg" },
+  { id: 4, name: "Stream", img: "/s.svg", nameImg: "/streamName.svg" },
+  { id: 5, name: "Docker", img: "/dock.svg", nameImg: "/dockerName.svg" },
+];
+
+// Work Experience with Enhanced Details
+export const workExperience = [
+  {
+    id: 1,
+    title: "User Interface (UI) Design",
+    desc: "Crafting visually appealing and intuitive interfaces tailored to your brand identity, Designing pixel-perfect layouts for web, mobile, and desktop applications, Ensuring consistency with style guides, color palettes, typography, and iconography. ",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "User Experience (UX) Design",
+    desc: "Conducting user research to understand your audience and their needs, Creating wireframes, prototypes, and user flows to map seamless user journeys, Performing usability testing to optimize product functionality and ease of use.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Responsive & Interactive Design",
+    desc: "Developing responsive designs that adapt seamlessly across devices and screen sizes, Implementing interactive elements to enhance user engagement and satisfaction, Utilizing modern design tools and frameworks for scalable and future-ready designs. ",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Design System Development",
+    desc: "Building comprehensive design systems to ensure consistency across all touchpoints, Providing reusable components, templates, and style libraries for efficient collaboration, Streamlining design and development processes for faster project delivery.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+// Social Media Links
+export const socialMedia = [
+  { id: 1, img: "/git.svg" },
+  { id: 2, img: "/twit.svg" },
+  { id: 3, img: "/link.svg" },
+];
